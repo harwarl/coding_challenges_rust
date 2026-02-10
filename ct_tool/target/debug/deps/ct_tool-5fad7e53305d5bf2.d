f7a2fecfe34166092335ed6d100f2a4401f2fb0a
harwarl <harwarl87@gmail.com>
2026-02-10 03:05:44 +0100

    setup cut tool

---
+++ ct_tool/target/debug/deps/ct_tool-5fad7e53305d5bf2.d
@@ -0,0 +1,5 @@
+/Users/dokun/Desktop/rust/coding_challenges_rust/ct_tool/target/debug/deps/ct_tool-5fad7e53305d5bf2.d: src/main.rs
+
+/Users/dokun/Desktop/rust/coding_challenges_rust/ct_tool/target/debug/deps/libct_tool-5fad7e53305d5bf2.rmeta: src/main.rs
+
+src/main.rs:
---
+++ ct_tool/target/debug/deps/ct_tool-efa0dc56d8f5eda2.d
@@ -0,0 +1,5 @@
+/Users/dokun/Desktop/rust/coding_challenges_rust/ct_tool/target/debug/deps/ct_tool-efa0dc56d8f5eda2.d: src/main.rs
+
+/Users/dokun/Desktop/rust/coding_challenges_rust/ct_tool/target/debug/deps/libct_tool-efa0dc56d8f5eda2.rmeta: src/main.rs
+
+src/main.rs:
