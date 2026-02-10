5d7aac94dc59d5325dad269e3f640905c42ce234
harwarl <harwarl87@gmail.com>
2026-02-10 03:29:17 +0100

    Clap setup

--- ct_tool/src/main.rs
+++ ct_tool/src/main.rs
@@ -1,3 +1,13 @@
+
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[command(name = "Cut Tool", version, about = "A tool for cutting things", long_about = None)]
+struct Args {
+
+}
+
 fn main() {
+    let args = Args::parse();
+    
     println!("Hello, world!");
 }
