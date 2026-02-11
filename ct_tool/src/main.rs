b5f6b20752f3657c2badfd510253b80e6a758579
harwarl <harwarl87@gmail.com>
2026-02-11 16:04:52 +0100

    Read file contents

--- ct_tool/src/main.rs
+++ ct_tool/src/main.rs
@@ -1,16 +1,32 @@
 
+use std::{fs::File, io::{self, BufReader, Read, read_to_string}, path::PathBuf};
+
 use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(name = "Cut Tool", version, about = "A tool for cutting things", long_about = None)]
 struct Args {
+    /// Fields to cut (1 - based)
+    #[arg(short, long)]
+    fields: usize,
 
+    /// path of file to read ,if not provided, it will read from stdin
+    #[arg(help = "path to file to check")]
+    file: Option<std::path::PathBuf>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    match args {
-        _ => println!("No Arguments Provided")
+    let file_contents = match args.file {
+        Some(path) => {
+            // Read in file as string
+            std::fs::read_to_string(path).expect("Error reading file")
+        },
+        None => {
+            let mut buffer = String::new();
+            io::stdin().read_to_string(&mut buffer).expect("Failed to read line");
+            buffer
+        }
     };
 
     println!("Hello, world!");
